d19e9f45e936e74f370f309594b649c42efed2c1
Bowie <fecastilhos@gmail.com>
2026-05-29 16:03:46 -0300

    feat: operacoes

--- main.dart
+++ main.dart
@@ -1,3 +1,4 @@
+
 main() {
   final listaConvertida = dadosTarefas.map((e) => Tarefa.fromMap(e)).toList();
 
@@ -11,8 +12,57 @@ main() {
       'Horas: ${element.horas}\n'
     ),
    );  
+
+  final tarefasConcluidas = listaConvertida.where((tarefa) => tarefa.status == 'concluida',).toList();
+  final tarefasPendentes = listaConvertida.where((tarefa) => tarefa.status == 'pendente',).toList();
+  final tarefasEmAndamento = listaConvertida.where((tarefa) => tarefa.status == 'em andamento',).toList();
+  final tarefasCancelada = listaConvertida.where((tarefa) => tarefa.status == 'cancelada',).toList();
+    
+    print('\nTarefas concluídas:');
+  tarefasConcluidas.forEach((element) {print('- ${element.titulo}');});
+
+    print('\nTarefas pendentes:');
+  tarefasPendentes.forEach((element) {print('- ${element.titulo}');});
+
+    print('\nTarefas em andamento:');
+  tarefasEmAndamento.forEach((element) {print('- ${element.titulo}');});
+
+    print('\nTarefas canceladas:');
+  tarefasCancelada.forEach((element) {print('- ${element.titulo}');});
+
+  double totalTarefasConcluidas = 0;
+  tarefasConcluidas.forEach((element) {totalTarefasConcluidas += element.valor ?? 0;});
+  print(
+  '\nTotal das tarefas concluídas: '
+  'R\$ ${totalTarefasConcluidas.toStringAsFixed(2)}');
+
+
+  double somaTarefasPendentes = 0;
+  tarefasPendentes.forEach((element){somaTarefasPendentes += element.valor ?? 0;});
+  int quantidadeTarefasPendentes = tarefasPendentes.length;
+  double mediaTarefasPendentes = quantidadeTarefasPendentes > 0 ? somaTarefasPendentes / quantidadeTarefasPendentes : 0;  
+
+  if (quantidadeTarefasPendentes > 0)
+    print (
+      '\nMédia das tarefas pendentes: '
+      'R\$ ${mediaTarefasPendentes.toStringAsFixed(2).replaceAll('.', ',')}');
+    else
+    print (
+      'Não existem tarefas pendentes para calcular média.');
+
+
+  Map<String, int> horasPorStatus = {};
+  listaConvertida.forEach((element) {
+  String status = element.status ?? 'Sem status';
+  int horas = element.horas ?? 0;
+
+  horasPorStatus[status] = (horasPorStatus[status] ?? 0) + horas;});
+  print('\nHoras por status:');
+  horasPorStatus.forEach((status, horas) {print('$status: $horas horas');});
   }
 
+
+  
 
 
 
